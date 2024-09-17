a0cc885b94935d1db6fd475c6266490d7dc51c95
SairaMehboob <sairamehboob702@gmail.com>
2024-09-17 11:22:29 +0500

    Exercise 43
    Typescript code

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-//DEFINING A FUNCTION WITH A REST PARAMETER THATR ACCEPTS BOTH ITEMS AND ARGUMENTS
-function makeSandwich(...items) {
-    console.log("MAKING A SANDWICH WITH THE FOLLOWING ITEMS:\n");
-    items.forEach(singleItem => console.log(singleItem));
-    console.log("\n NOW ENJOY YOUR SANDWICH.");
+//DEFINING A FUNCTION TO PRINT EACH MAGICIAN'S NAME
+function show_magicians(magicians) {
+    magicians.forEach(name => console.log(name));
 }
-//CALLING THE FUNCTION WITH THREE DIFFERENT NUMBER OF ARGUMENTS
-makeSandwich("CHICKEN", "CHEESE", "MAYO", "EGG");
-makeSandwich("BREAD", "BUTTER");
-makeSandwich("BREAD", "BUTTER", "EGG", "MAYO", "LETTUCE", "CHEESE", "CHICKEN", "TOMATO");
+//DEFINING AN ARRAY CONTAINING MAGICIANS NAME
+function make_great(magicians) {
+    return magicians.map(name => `THE GREAT ${name} `);
+}
+//DEFINING AN AARAY OF MAGICIANS NAMES
+let magician_names = ["HARRY POTTER", "DAVID", "HARRY HOUDINI", "DOUG HENNING"];
+//MAKING COPY OF ORIGINAL ARRAY THROUGH SLICE FUNCTION
+let copy_magician_names = magician_names.slice();
+//MODIFYING BY ADDIND WORD"THE GREAT"WITH THE NAMES IN ARRAY
+let copy_great_magicians = make_great(copy_magician_names);
+//SHOWING BOTH ORIGINAL AND COPIED ARRAYS
+//ORIGINAL:-
+show_magicians(magician_names);
+//COPIED:-
+show_magicians(copy_great_magicians);
