3bb258ba5cb8955e500c67087322cad3107b61fc
SairaMehboob <sairamehboob702@gmail.com>
2024-09-17 11:23:29 +0500

    Exercise 42
    Typescript code

--- main.js
+++ main.js
@@ -8,14 +8,6 @@ function show_magicians(magicians) {
 function make_great(magicians) {
     return magicians.map(name => `THE GREAT ${name} `);
 }
-//DEFINING AN AARAY OF MAGICIANS NAMES
 let magician_names = ["HARRY POTTER", "DAVID", "HARRY HOUDINI", "DOUG HENNING"];
-//MAKING COPY OF ORIGINAL ARRAY THROUGH SLICE FUNCTION
-let copy_magician_names = magician_names.slice();
-//MODIFYING BY ADDIND WORD"THE GREAT"WITH THE NAMES IN ARRAY
-let copy_great_magicians = make_great(copy_magician_names);
-//SHOWING BOTH ORIGINAL AND COPIED ARRAYS
-//ORIGINAL:-
-show_magicians(magician_names);
-//COPIED:-
-show_magicians(copy_great_magicians);
+let greatMagicians = make_great(magician_names);
+show_magicians(greatMagicians);
