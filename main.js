d6c3fd0419ad82058a08ba82f00a56931deb41dc
SairaMehboob <sairamehboob702@gmail.com>
2024-09-17 11:29:19 +0500

    Exercise 36

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// MAKING A FUNCTION
-function make_shirt(size = "LARGE", printMessage = "IN LOVE WITH TYPESCRIPT") {
-    console.log(`CREATING A ${size} SHIRT WITH THE ${printMessage} PRINTS ON It!`);
+function make_shirt(size, printMessage) {
+    console.log(`YOU SELECTED ${size} SIZE SHIRT WITH ${printMessage} PRINTS ON THE SHIRT!`);
 }
-//CALLING A FUNCTION WITH BY-DEFAULT VALUES
-make_shirt();
-//NOW CALLING A FUNCTION WITH MEDIUM SIZE AND DEFAULT MESSAGE
-make_shirt("MEDIUM");
-//NOW CALLING A FUNCTION WITH SMALL SIZE AND ALSO PRINTING DIFFERENT MESSAGE
-make_shirt("SMALL", "I LOVE JAVASCRIPT:)");
+make_shirt("MEDIUM", "CODE WITH ME");
