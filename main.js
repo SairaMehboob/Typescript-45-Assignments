164c4633bfc0cbc1aa8a6486a372e86225957fbc
SairaMehboob <sairamehboob702@gmail.com>
2024-09-17 11:31:05 +0500

    Exercise 33

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-//CREATING AN ARRAY
-let pizza = ["CHICKEN TIKKA", "MALAI CHEESE", "FEJITA"];
+//CREATING AN ARRAY 
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 //USING FOR-LOOP
-for (let onePizza of pizza) {
-    console.log(`I LIKE ${onePizza} PIZZA`);
+for (let oneNumber of numbers) {
+    let ordinalEnding;
+    if (oneNumber === 1) {
+        ordinalEnding = "st";
+    }
+    else if (oneNumber === 2) {
+        ordinalEnding = "nd";
+    }
+    else if (oneNumber === 3) {
+        ordinalEnding = "rd";
+    }
+    else if (oneNumber === 4) {
+        ordinalEnding = "th";
+    }
+    else if (oneNumber === 5) {
+        ordinalEnding = "th";
+    }
+    else {
+        ordinalEnding = "th";
+    }
+    console.log(`${oneNumber}${ordinalEnding}`);
 }
-//PRINTING A MESSAGE OUTSIDE OF THE LOOP
-console.log("MY LOVE PIZZA!");
