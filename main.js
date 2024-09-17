cb0cc2b8710331209417032d439355403fefc74d
SairaMehboob <sairamehboob702@gmail.com>
2024-09-17 11:29:55 +0500

    Exercise 35

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function make_shirt(size, printMessage) {
-    console.log(`YOU SELECTED ${size} SIZE SHIRT WITH ${printMessage} PRINTS ON THE SHIRT!`);
+//CREATING AN ARRAY
+let petAnimals = ["AYMAN", "DOG", "RABBIT"];
+//USING FOR-LOOP
+for (let onePet of petAnimals) {
+    console.log(`A ${onePet} WOULD BE A GREAT PET.`);
 }
-make_shirt("MEDIUM", "CODE WITH ME");
+//PRINTING A MESSAGE OUTSIDE OF FOR LOOP
+console.log("ANY OF THESE ANIMALS WOULD BE A GREAT PET!");
