1cf33b1e3af80d8f2b8381e8541bbf51ebbb48fb
SairaMehboob <sairamehboob702@gmail.com>
2024-09-17 11:32:33 +0500

    Exercise 30

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+//CREATING AN ARRAY
 let userNames = ["MARIA", "ADEEBA", "ADMIN", "SADIA", "NAZIA"];
-if (userNames.length === 0) {
-    console.log("YOUR ARRAY IS EMPTY! WE NEED TO FIND SOME USERS.");
-}
-else {
-    userNames.forEach(oneUser => {
-        if (oneUser === "ADMIN") {
-            console.log(`HELLO! ${oneUser}, WOULD YOU LIKE TO SEE THE STATUS REPORT?`);
-        }
-        else {
-            console.log(`HELLO! ${oneUser},THANKYOU FOR LOGGING IN AGAIN.`);
-        }
-    });
-}
+//USING FOREACH LOOP IN THE ARRAY
+userNames.forEach(oneUser => {
+    if (oneUser === "ADMIN") {
+        console.log("HELLO! ${oneUser}, WOULD YOU LIKE TO SEE THE STATUS REPORT?");
+    }
+    else {
+        console.log("HELLO! ${oneUser},THANKYOU FOR LOGGING IN AGAIN.");
+    }
+});
