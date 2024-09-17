28d6dba541042d554bd0dcff50aea8d9c8808c9f
SairaMehboob <sairamehboob702@gmail.com>
2024-09-17 11:24:19 +0500

    Exercise 41
    Typescript code

--- main.js
+++ main.js
@@ -5,9 +5,6 @@ function show_magicians(magicians) {
     magicians.forEach(name => console.log(name));
 }
 //DEFINING AN ARRAY CONTAINING MAGICIANS NAME
-function make_great(magicians) {
-    return magicians.map(name => `THE GREAT ${name} `);
-}
 let magician_names = ["HARRY POTTER", "DAVID", "HARRY HOUDINI", "DOUG HENNING"];
-let greatMagicians = make_great(magician_names);
-show_magicians(greatMagicians);
+//CALLING THE FUNCTION TO PINT EACH MAGICIAN'S NAME
+show_magicians(magician_names);
