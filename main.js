c3f06f073469e8ec52f92e5c220f10ee9090e3ee
SairaMehboob <sairamehboob702@gmail.com>
2024-09-17 11:28:40 +0500

    Exercise 37

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-//MAKING A FUNCTION
-function describe_city(city, country = "PAKISTAN") {
-    console.log(`${city} IS IN ${country}.`);
+// MAKING A FUNCTION
+function make_shirt(size = "LARGE", printMessage = "IN LOVE WITH TYPESCRIPT") {
+    console.log(`CREATING A ${size} SHIRT WITH THE ${printMessage} PRINTS ON It!`);
 }
-//CALLING THE FUNCTION
-describe_city("KARACHI");
-describe_city("LAHORE");
-describe_city("BERLIN", "GERMANY");
+//CALLING A FUNCTION WITH BY-DEFAULT VALUES
+make_shirt();
+//NOW CALLING A FUNCTION WITH MEDIUM SIZE AND DEFAULT MESSAGE
+make_shirt("MEDIUM");
+//NOW CALLING A FUNCTION WITH SMALL SIZE AND ALSO PRINTING DIFFERENT MESSAGE
+make_shirt("SMALL", "I LOVE JAVASCRIPT:)");
