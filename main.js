1066ddd053cc579a21a15d02f97afc48fb590d12
SairaMehboob <sairamehboob702@gmail.com>
2024-09-17 11:21:09 +0500

    Exercise 44
    Typescript Assignment

--- main.js
+++ main.js
@@ -1,20 +1,12 @@
-//DEFINING A FUNCTION
-function create_car(manufacturer, model) {
-    var options = [];
-    for (var _i = 2; _i < arguments.length; _i++) {
-        options[_i - 2] = arguments[_i];
-    }
-    var car = {
-        manufacturer: manufacturer,
-        model: model
-    };
-    //ADDING ADDITIONAL OPTIONS TO THE CAR OBJECT
-    options.forEach(function (option) {
-        var _a = option.split(":"), key = _a[0], value = _a[1];
-        car[key.trim()] = value.trim();
-    });
-    return car;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//DEFINING A FUNCTION WITH A REST PARAMETER THATR ACCEPTS BOTH ITEMS AND ARGUMENTS
+function makeSandwich(...items) {
+    console.log("MAKING A SANDWICH WITH THE FOLLOWING ITEMS:\n");
+    items.forEach(singleItem => console.log(singleItem));
+    console.log("\n NOW ENJOY YOUR SANDWICH.");
 }
-//CALLING THE FUNCTION TO CREATE A CAR OBJECT
-var myCar = create_car("TOYOTA", "COROLLA", "COLOR:BLACK", "SUNROOF:TRUE");
-console.log(myCar);
+//CALLING THE FUNCTION WITH THREE DIFFERENT NUMBER OF ARGUMENTS
+makeSandwich("CHICKEN", "CHEESE", "MAYO", "EGG");
+makeSandwich("BREAD", "BUTTER");
+makeSandwich("BREAD", "BUTTER", "EGG", "MAYO", "LETTUCE", "CHEESE", "CHICKEN", "TOMATO");
