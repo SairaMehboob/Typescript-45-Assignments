bd2fc1a802ad0af85175b41895659763f7879ccd
SairaMehboob <sairamehboob702@gmail.com>
2024-09-17 11:30:31 +0500

    Exercise 34

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 //CREATING AN ARRAY
-let petAnimals = ["AYMAN", "DOG", "RABBIT"];
+let pizza = ["CHICKEN TIKKA", "MALAI CHEESE", "FEJITA"];
 //USING FOR-LOOP
-for (let onePet of petAnimals) {
-    console.log(`A ${onePet} WOULD BE A GREAT PET.`);
+for (let onePizza of pizza) {
+    console.log(`I LIKE ${onePizza} PIZZA`);
 }
-//PRINTING A MESSAGE OUTSIDE OF FOR LOOP
-console.log("ANY OF THESE ANIMALS WOULD BE A GREAT PET!");
+//PRINTING A MESSAGE OUTSIDE OF THE LOOP
+console.log("MY LOVE PIZZA!");
